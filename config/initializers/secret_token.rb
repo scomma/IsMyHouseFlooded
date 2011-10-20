edc2f298572e6d5f74b8795aa58a5c0a600f5e61
Prathan Thananart <prathan@oozou.com>
2011-10-20 21:46:45 +0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ismyhouseflooded::Application.config.secret_token = '56746dca45436aab41256759927519e589d389cf52171dd9b203fd7fae57eff9ab8254c55798d5739dfa6e8b8ba127ae1bf204841e49dd4bd5bd99bc6028f9cc'
