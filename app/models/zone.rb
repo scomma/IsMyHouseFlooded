368b8139516977834c41adb4deb36b9156d75226
Prathan Thananart <prathan@oozou.com>
2011-10-21 14:43:51 +0700

    Validate postal code

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,4 +1,6 @@
 class Zone < ActiveRecord::Base
+  validates :id, length:{ is: 5 }, numericality:{ greater_than: 10000 } # valid postal code in Thailand
+
   has_many :reports
 
   def positive_percent
