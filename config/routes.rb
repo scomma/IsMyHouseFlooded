619b47b3347274babe0ad0b518a169d975c914c0
Prathan Thananart <prathan@oozou.com>
2011-10-24 09:15:14 +0700

    Precompile rails_admin assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Ismyhouseflooded::Application.routes.draw do
-  root to: 'zones#index', as: 'zones'
-  resources :zones, only: []
+  root to: 'zones#index'
+  resources :zones, only: [:index, :show], path: ''
   resources :reports, only: [:new, :create]
   post 'reports/new' => 'reports#new'
 
