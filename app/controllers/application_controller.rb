998a7c81865ed54a2ff70da1e5262011e143aa2f
Kridsada Thanabulpong <sirn@ogsite.net>
2011-10-25 00:24:17 +0700

    Read admin username/password from env [close #1]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,18 @@ class ApplicationController < ActionController::Base
 
   protected
     def authenticate_admin!
+      if Rails.env.production?
+        credentials = {
+          username: ENV['ADMIN_USERNAME'],
+          password: ENV['ADMIN_PASSWORD_HASH']}
+      else
+        credentials = {
+          username: "noah",
+          password: "6057c39ce781ea2808fe4d1edb1c8610d07ef772"}
+      end
       authenticate_or_request_with_http_basic do |username, password|
-        username == "noah" and
-        Digest::SHA1.hexdigest(password) == "6057c39ce781ea2808fe4d1edb1c8610d07ef772"
+        username == credentials[:username] and
+        Digest::SHA1.hexdigest(password) == credentials[:password]
       end
     end
 
