b6df1f02e0a02eb08f4076070de4859e0df054e5
Prathan Thananart <prathan@oozou.com>
2011-10-25 02:32:34 +0700

    Try to get AssetSync to work

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -2,6 +2,6 @@ AssetSync.configure do |config|
   config.aws_access_key = ENV['AWS_ACCESS_KEY']
   config.aws_access_secret = ENV['AWS_ACCESS_SECRET']
   config.aws_bucket = ENV['AWS_BUCKET']
-  config.aws_region = "us-west-1"
+  # config.aws_region = "us-west-1"
   config.existing_remote_files = "delete"
 end
