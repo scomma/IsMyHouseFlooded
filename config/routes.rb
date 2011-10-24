005872eef2bb648a45ef9a8e6f758ce60057438f
Prathan Thananart <prathan@oozou.com>
2011-10-24 15:26:26 +0700

    Fix mislocated assets in some browsers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Ismyhouseflooded::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  get 'fancybox/:asset' => redirect("/assets/%{asset}"), constraints:{ asset: /.+/ }
 
   resources :reports, only: [:new, :create]
   post 'reports/new' => 'reports#new'
