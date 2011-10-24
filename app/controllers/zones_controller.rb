6db9a61867dd7757b057b8d7f28e6842c2372a5b
Prathan Thananart <prathan@oozou.com>
2011-10-24 12:54:33 +0700

    Show insignificant zones when searched for

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -1,6 +1,6 @@
 class ZonesController < ApplicationController
   def index
-    @zones = Zone.order{updated_at.desc}.where{reports_count > 1}.all
+    @zones = Zone.order{updated_at.desc}.all
     @report = Report.new
 
     headers['Cache-Control'] = 'public, max-age=60'
