2dc7d6908896f6cbbb499edb260e337181c10b39
Prathan Thananart <prathan@oozou.com>
2011-10-25 04:12:19 +0700

    Add cache for static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def show
+    headers['Cache-Control'] = 'public, max-age=300'
     render params[:id], layout: if request.xhr? || params[:format] == 'partial' then false else true end
   end
 end
