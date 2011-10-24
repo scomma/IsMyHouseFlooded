69f88a12193f9416697cd52c4d2abee26fffbd4a
Prathan Thananart <prathan@oozou.com>
2011-10-24 11:44:16 +0700

    Enable http caching

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -2,10 +2,14 @@ class ZonesController < ApplicationController
   def index
     @zones = Zone.order{updated_at.desc}.where{reports_count > 1}.all
     @report = Report.new
+
+    headers['Cache-Control'] = 'public, max-age=60'
   end
 
   def show
     @zone = Zone.includes(:reports).find params[:id]
+
+    headers['Cache-Control'] = 'public, max-age=60'
     render layout: false if request.xhr?
   end
 
